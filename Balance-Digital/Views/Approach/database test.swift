5338983b66e955d184aa955fac59bb0bedd7b4f3
Bryan_Dev <bryan_dev@Mac-de-Bryan-Dev.local>
2026-04-30 10:09:14 -0600

    Agregacion Backen Login y registro

---
+++ Balance-Digital/Views/Approach/database test.swift
@@ -0,0 +1,8 @@
+//
+//  database test.swift
+//  Balance-Digital
+//
+//  Created by Bryan_Dev on 30/4/26.
+//
+
+import Foundation
---
+++ Balance-Digital/Views/Approach/test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  Balance-Digital
+//
+//  Created by Bryan_Dev on 30/4/26.
+//
+
+import Foundation
