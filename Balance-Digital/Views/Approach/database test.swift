13e3f022333e04e3bcc5e189f728c5df9f78d85d
Bryan_Dev <bryan_dev@Mac-de-Bryan-Dev.local>
2026-04-30 10:24:23 -0600

    Agregacion nuevas carpetas

--- Balance-Digital/Views/Approach/database test.swift
+++
@@ -1,8 +0,0 @@
-//
-//  database test.swift
-//  Balance-Digital
-//
-//  Created by Bryan_Dev on 30/4/26.
-//
-
-import Foundation
--- Balance-Digital/Views/Approach/test.swift
+++
@@ -1,8 +0,0 @@
-//
-//  test.swift
-//  Balance-Digital
-//
-//  Created by Bryan_Dev on 30/4/26.
-//
-
-import Foundation
